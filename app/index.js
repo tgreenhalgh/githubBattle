730de2bc493a2287af5e12cc0e0f8946e3ab80ab
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2018-04-23 16:28:15 -0700

    some setup

---
+++ app/index.js
@@ -0,0 +1,41 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+require('./index.css');
+
+class Users extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Friends</h1>
+        <ul>
+          {this.props.list
+            .filter(f => f.friend)
+            .map(el => <li key={el.name}>{el.name}</li>)}
+        </ul>
+
+        <hr />
+
+        <h1> Non Friends </h1>
+        <ul>
+          {this.props.list
+            .filter(f => !f.friend)
+            .map(el => <li key={el.name}>{el.name}</li>)}
+        </ul>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Users
+    list={[
+      { name: 'Tyler', friend: true },
+      { name: 'Ryan', friend: true },
+      { name: 'Michael', friend: false },
+      { name: 'Mikenzi', friend: false },
+      { name: 'Jessica', friend: true },
+      { name: 'Dan', friend: false },
+    ]}
+  />,
+  document.getElementById('app'),
+);
