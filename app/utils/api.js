e24209a49567ea45a1884a9a986f3d72a7a09d5c
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2018-04-30 18:46:54 -0700

    Adds helper functions in API

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,6 +1,59 @@
 const axios = require('axios');
 
+function getProfile(username) {
+  return axios
+    .get('https://api.github.com/users/' + username)
+    .then(user => user.data);
+}
+
+function getRepos(username) {
+  return axios.get(
+    'https://api.github.com/users/' + username + '/repos?&per_page=100',
+  );
+}
+
+function getStarCount(repos) {
+  return repos.data.reduce((count, repo) => count + repos.stargazers_count, 0);
+}
+
+function calculateScore(profile, repos) {
+  let follower = profile.followers;
+  let totalStars = getStarCount(repos);
+
+  return followers * 3 + totalStars;
+}
+
+function handleError(err) {
+  console.warn(err);
+  return null;
+}
+
+function getUserData(player) {
+  return axios.all([getProfile(player), getRepos(player)]).then(data => {
+    let profile = data[o];
+    let repost = data[1];
+
+    return {
+      profile,
+      score: calculateScore(profile, repos),
+    };
+  });
+}
+
+function sortPlayers(players) {
+  return players.sort(function(a, b) {
+    return b.score - a.score;
+  });
+}
+
 module.exports = {
+  battle: players =>
+    axios.all(
+      players
+        .map(getUserData)
+        .then(sortPlayers)
+        .catch(handleError),
+    ),
   fetchPopularRepos: language => {
     let encodedURI = window.encodeURI(
       'https://api.github.com/search/repositories?q=stars:>1+language:' +
